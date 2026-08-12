b7ccb376ff51783731e45ae3e327a3a406c26c09
Aakash Kumar Raut <24BCS12826@cuchd.in>
2026-08-13 00:56:24 +0530

    Time: 24 ms (62.21%), Space: 139.7 MB (49.75%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -2,16 +2,22 @@ class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         int sum=0;
         HashMap<Integer,Integer> map=new HashMap<>();
-          map.put(0,1);
+          map.put(0,-1);
+        // Remainder 0 exists before the array starts
+        // This helps detect subarrays starting from index 0
         for(int i=0; i<nums.length; i++){
            sum+=nums[i];
             int rem=sum % k;
+             // If this remainder was seen before,
+            // the elements between the two indexes have sum divisible by k
             if(map.containsKey(rem)){
-              if(i - map.get(rem) >=2){
+                // Check that subarray has at least 2 elements
+              if(i - map.get(rem) >=2){//simply finding idex value - previous idex value who get rem if condition stisfied it means already 
+              
                 return true;
               }
              
-            } else{
+            } else{// Store only the first/earliest index of this remainder
                 map.put(rem,i);
               }
         }
